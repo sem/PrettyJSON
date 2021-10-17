b4137ef51cdf20f515e5e6a9310dca708ac34c2e
Sem <78478073+semmoolenschot@users.noreply.github.com>
2021-10-17 19:07:01 +0200

    2nd commit

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-bot = commands.Bot(command_prefix='.', intents= discord.Intents.all())
+bot = commands.Bot(command_prefix='.', intents=discord.Intents.all())
   
 @bot.command()
 async def load(ctx, extension):
@@ -13,4 +13,4 @@ async def unload(ctx, extension):
 
 bot.load_extension('cog') # Loading cog by default without need of commands.
 
-bot.run('ODk4OTMzODI1NBF2MDczMjg2.YWraXQ.F9kMt4oafigdIGfzZ_N0GTXmcok')
+bot.run('ODk4OTMzODI1NBF2MDczMjg2.YWraXQ.F9kMt4oafigdIGfzZ_N0GTXmcok') # Token
