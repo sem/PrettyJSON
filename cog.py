870b6c53f56d52f7c9f11c872b52412b8c2435d6
Sem <78478073+semmoolenschot@users.noreply.github.com>
2021-10-17 21:31:56 +0200

    Update cog.py
    Small bug fix and changed the command from 'pretty_print' to 'pprint'.

--- cog.py
+++ cog.py
@@ -31,7 +31,7 @@ class Cog(commands.Cog):
             await message.channel.send(file=discord.File('output.json'))
             await message.remove_reaction('\U0001f4c1', self.bot.user)
 
-    @commands.command()
+    @commands.command(aliases=['pprint'])
     async def pretty_print(self, ctx : commands.Context, *, args=None):
         if args != None: args = args.replace('“', '"').replace('”','"')
         if len(ctx.message.attachments) == 0 and len(args) > 0 and not args.startswith('http'):
@@ -52,17 +52,23 @@ class Cog(commands.Cog):
             except(Exception):
                 await ctx.send('Sorry, but valid JSON could not be obtained from that link, maybe try pretty printing with a file of JSON?')
                 return
+        input_data = open('output.json', 'r', encoding='utf8').read()
         data = open('output.json', 'r')
+        with open('output.json', 'w') as f: 
+            f.write(input_data.replace('“', '"').replace('”', '"'))
         try: 
             json_data = json.load(data)
-        except (JSONDecodeError):
+        except(JSONDecodeError) as e:
             await ctx.send('The data that was inserted could not be converted to valid JSON. Make sure your data contains the right JSON syntax.')
+            print(e.msg)
             return
         with open('output.json', 'w') as f:
             json.dump(json_data, f, indent=4)
-        if len(data.read()) > 2000: msg_sent = await ctx.send(file=discord.File('output.json'))
+        if len(input_data) > 2000:
+            msg_sent = await ctx.send(file=discord.File('output.json'))
         else: 
             msg_sent = await ctx.send('```json\n' + open('output.json', 'r').read() + '```')
             await msg_sent.add_reaction('\U0001f4c1')
 
-def setup(client): client.add_cog(Cog(client))
+def setup(client):
+    client.add_cog(Cog(client))
